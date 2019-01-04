69b363d826ab27a38f4a80561ae51ddb45c5c834
anjaligeril <anjali99malu@gmail.com>
2019-01-03 22:31:08 -0500

    first commit

--- src/containers/SkillsCloud.js
+++ src/containers/SkillsCloud.js
@@ -5,9 +5,11 @@ import CloudItem from '../components/CloudItem';
 import './css/CloudSkillsCSS.css';
 import Video from "./images/background.mp4";
 import Cv from "./images/cv.pdf";
-import Image from'./images/img1.jpg'
+import Image1 from'./images/img4.png'
+import Image2 from'./images/img3.jpg'
+import Image3 from './images/Img5.png'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
+import BackgroundSlideshow from 'react-background-slideshow'
 
 const styles = {
     large: {
@@ -33,79 +35,75 @@ class SkillsCloud extends Component {
     render() {
         return (
             <div className="SkillsCloudBody ">
-                <div className="mainBody">
+                <BackgroundSlideshow images={[ Image1, Image2, Image3 ]} />
                 <ReactCSSTransitionGroup
                     transitionName="fade"
                     transitionAppear={true}
                     transitionAppearTimeout={2500}
                     transitionEnterTimeout={2500}
                     transitionLeaveTimeout={3000}>
-                    <video autoPlay muted loop className="video1" >
-                        <source src={Video} type="video/mp4" />
-                    </video>
-
-                <div className="skills1">
-                  <h2>Hi.. I am Anjali Elizabeth Joseph </h2>
-                    <h4>I am an Aspiring programmer</h4>
-
-                </div>
-
-                <div className="SkillsCloud-outer">
 
 
-                    <div className="col-lg-3"></div>
+                    <div className="skills1">
+                        <h2>Hi. I’m Anjali, a software developer from Toronto, Canada.</h2> <h3>Please take a look around!</h3>
+                    </div>
 
-                    <div className="SkillsCloud-inner col-lg-6">
+                    <div className="SkillsCloud-outer">
 
-                        <TagCloud
-                            className='tag-cloud'
-                            style={{
-                                fontFamily: 'sans-serif',
-                                fontSize: 30,
-                                color: () => randomColor({
-                                    hue: 'blue'
-                                }),
-                                padding: 5,
-                            }}>
-                            <div
+
+                        <div className="col-lg-3"></div>
+
+                        <div className="SkillsCloud-inner col-lg-6">
+
+                            <TagCloud
+                                className='tag-cloud'
                                 style={{
-                                    fontFamily: 'serif',
-                                    fontSize: 40,
-                                    fontStyle: 'italic',
-                                    fontWeight: 'bold',
-                                    color: randomColor()
-                                }}>Photoshop</div>
-                            <div> </div>
-                            <div >JavaScript</div>
-                            <div >HTML5</div>
-                            <div >CSS</div>
-                            <div >React.js</div>
-                            <div >BootStrap</div>
-                            <div >Java</div>
-                            <div >C</div>
-                            <div >C++</div>
-                            <div >C#</div>
-                            <div >ASP.net</div>
-                            <div >Python</div>
-                            <div>JSP</div>
-                            <div>SQL</div>
-                            <div>TSQL</div>
-                            <div>JSON</div>
-                            <div>XML</div>
-                            <div >Angular.js</div>
+                                    fontFamily: 'sans-serif',
+                                    fontSize: 30,
+                                    color: () => randomColor({
+                                        hue: 'blue'
+                                    }),
+                                    padding: 5,
+                                }}>
+                                <div
+                                    style={{
+                                        fontFamily: 'serif',
+                                        fontSize: 40,
+                                        fontStyle: 'italic',
+                                        fontWeight: 'bold',
+                                        color: randomColor()
+                                    }}>Photoshop</div>
+                                <div> </div>
+                                <div >JavaScript</div>
+                                <div >HTML5</div>
+                                <div >CSS</div>
+                                <div >React.js</div>
+                                <div >BootStrap</div>
+                                <div >Java</div>
+                                <div >C</div>
+                                <div >C++</div>
+                                <div >C#</div>
+                                <div >ASP.net</div>
+                                <div >Python</div>
+                                <div>JSP</div>
+                                <div>SQL</div>
+                                <div>TSQL</div>
+                                <div>JSON</div>
+                                <div>XML</div>
+                                <div >Angular.js</div>
 
-                        </TagCloud>
+                            </TagCloud>
+
+                        </div>
+
+                        <div className="col-lg-3"></div>
 
                     </div>
-                    
-                    <div className="col-lg-3"></div>
-
-                </div>
 
 
-                <div className="headButton ">
+                    <div className="headButton ">
 
-                    <a className="link1" href="mailto:anjali99malu@gmail.com">anjali99malu@gmail.com</a>
+                        <a className="link1" href="mailto:anjali99malu@gmail.com">anjali99malu@gmail.com</a>
                         <a className="link1" target="_blank" href={Cv}>Resume  <i
                             className="fa fa-download"></i></a>
 
@@ -116,13 +114,11 @@ class SkillsCloud extends Component {
 
 
 
-                       <a className="link1"  target="_blank" href="https://github.com/anjaligeril"> GitHub <i
+                        <a className="link1"  target="_blank" href="https://github.com/anjaligeril"> GitHub <i
                             className="fa fa-github"></i></a>
 
-                </div>
-
+                    </div>
                 </ReactCSSTransitionGroup>
-                </div>
             </div>
         );
     }
