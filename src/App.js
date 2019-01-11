7f5b7e7e3ef1ea6f6976de4adf0c5a525ca61b72
anjaligeril <anjali99malu@gmail.com>
2019-01-10 22:40:36 -0500

    update jan10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import PortFolio from './containers/PortFolio'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <BrowserRouter><PortFolio/></BrowserRouter>
+          <HashRouter><PortFolio/></HashRouter>
       </div>
     );
   }
