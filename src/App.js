40453f623dc02212500a26116a659b833f07ac9b
anjaligeril <anjali99malu@gmail.com>
2019-01-11 11:02:38 -0500

    update browserrouter back

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import PortFolio from './containers/PortFolio'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <HashRouter><PortFolio/></HashRouter>
+          <BrowserRouter><PortFolio/></BrowserRouter>
       </div>
     );
   }
